64e9d7e1e159aed9a48327531520aa87b667de15
Rohi0524 <rohithugar81@gmail.com>
2023-06-08 13:09:35 +0530

    test

---
+++ wp-content/themes/ckarla/framework/color.php
@@ -0,0 +1,107 @@
+
+.navbar-light .navbar-nav .active > .nav-link, .navbar-light .navbar-nav .nav-link.active, .navbar-light .navbar-nav .nav-link.show, .navbar-light .navbar-nav .show > .nav-link{
+  color: #519687;
+}
+.nav-item:hover .nav-link{
+  color: #519687 !important;
+}
+.nav-item:after{
+  background-color: #519687;
+}
+.totop{
+  background-color: #519687;
+}
+.section-padding span{
+  color: #519687;
+}
+.line-hr-left{
+  border-color: #519687;
+}
+.karla-about-img .img:before{
+  border-color: #519687;
+}
+.line-hr-center{
+  border-color: #519687;
+}
+.recent-weddings .item .content h5 a:hover{
+  color: #519687;
+}
+.footer-section .footer-top .left-cont li:hover, .footer-section .footer-top .left-cont li a:hover{
+  color: #519687;
+}
+.footer-section .footer-top .right-cont li:hover, .footer-section .right-cont li a:hover{
+  color: #519687;
+}
+.travel-dates-table tr{
+  border-color: #519687;
+}
+.prices .item .prices-img .cat{
+  background: #519687;
+}
+.ti-check{
+  color: #519687;
+}
+.ti-close{
+  color: #519687;
+}
+.exstra-services h5 .price{
+  color: #519687;
+}
+.karla-gallery-toolbar li.active a{
+  background-color: #519687;
+}
+.faqs-accordion .accordion .active .title{
+  background: #519687;
+}
+.blog-page .post-cont .tag, .blog-page .post-cont .date{
+  color: #519687;
+}
+.blog-page .post-cont h5 a:hover{
+  color: #519687;
+}
+ul.pagination span.current{
+  background-color: #519687;
+  border-color: #519687;
+}
+ul.pagination li a:hover{
+  background-color: #519687;
+  border-color: #519687;
+}
+.blog-sidebar .widget ul li a:hover{
+  color: #519687;
+}
+.blog-sidebar ul.wp-tag-cloud li{
+  background-color: #519687;
+}
+.line-one{
+  background-color: #519687;
+}
+input:focus, textarea:focus{
+  border-color: #519687;
+}
+input.karla-btn{
+  background: #519687;
+  border-color: #519687;
+}
+input.karla-btn:hover{
+  color: #519687 !important;
+}
+.karla-post-section span{
+  color: #519687;
+}
+.karla-post-section span{
+  color: #519687;
+}
+#form input.submit{
+  background: #519687;
+  border-color: #519687;
+}
+#form input.submit:hover{
+  color: #519687;
+}
+body.sidebar  #ckarla-aside .ckarla-main-menu > ul > li > a:hover, body.sidebar  #ckarla-aside .ckarla-main-menu > ul > li.active > a, body.sidebar  #ckarla-aside .ckarla-main-menu > ul > li.open > a{
+  color: #519687;
+}
+body.sidebar  #ckarla-aside .ckarla-footer ul li:hover{
+  border-color: #519687;
+}
